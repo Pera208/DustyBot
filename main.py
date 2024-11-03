fe208b23edcdb9a612d25de504c88adff4d393e9
Pera <89399554+Pera208@users.noreply.github.com>
2024-11-03 22:32:52 +0700

    fixed bug that caused the bot to not response

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
 # Step 1: Bot setup
 intents = discord.Intents.default()
-intents.messages_content = True
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 # Step 2: Message functionality
@@ -18,7 +18,7 @@ async def send_message(message, user_message: str) -> None:
     if not user_message:
         print('Message was empty because intents were not enabled probably')
         return
-    if is_private := user_message[0] == '?p':
+    if is_private := '?p' in user_message:
         user_message = user_message[2:]
 
     try:
@@ -33,8 +33,14 @@ async def on_ready() -> None:
     print(f'{client.user} has connected to Discord!')
 
 # Step 4: Handling messages
+@client.event
 async def on_message(message) -> None:
     if message.author == client.user:
+        return
+
+    dumlist = [853586555289206814, 803765294488616960, 1095643735603298384, 791482512773873734]
+    if message.author.id in dumlist:
+        await message.channel.send("ดำ ไม่อยากคุยด้วย")
         return
 
     username: str = str(message.author)
